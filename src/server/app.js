1145c711fd3062912f0e49eb2c9773e302c86c18
Anton Björkman <antonidag@hotmail.com>
2024-07-04 10:45:25 +0200

    add http server

---
+++ src/server/app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import ollama from 'ollama';
+import cors from 'cors';
+
+const app = express();
+const port = 8080;
+const MODEL = 'qwen2:0.5b';
+
+app.use(cors()); // Apply CORS middleware
+app.use(express.json()); // Support JSON-encoded bodies
+app.use(express.urlencoded({ extended: true })); // Support URL-encoded bodies
+
+app.options('*', cors()); // Handle preflight requests for all routes
+
+app.post('/ai/chat', async (req, res) => {
+    const message = { role: 'user', content: req.body.prompt };
+    const response = await ollama.chat({ model: MODEL, messages: [message], stream: true });
+    for await (const part of response) {
+        res.write(part.message.content);
+        console.log(part.message.content);
+    }
+    res.end();
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
